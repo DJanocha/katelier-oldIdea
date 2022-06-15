3a629cd2d838a2b3dfee2e9e37b15760852430e1
przykladowy uzytkownik <przykladowy@email.com>
2022-06-15 02:14:08 +0200

    code formatted with prettier

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,19 @@
-import express, {Request, Response} from 'express'
-import dotenv from 'dotenv'
-import path from 'path'
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
 
 //dotenv nie pobiera sie poprawnie. do naprawy potem
-dotenv.config({path: path.join(__dirname, '..','.env')});
-
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
 const app = express();
 
 const port = process.env.SERVER_PORT || 1234;
-console.log(process.env)
+console.log(process.env);
 
-app.get('/', (req: Request, res: Response)=>{
-    res.json({ok: true})
-})
+app.get('/', (req: Request, res: Response) => {
+  res.json({ ok: true });
+});
 
-app.listen(port, ()=>{console.log(`running on port ${port}`)})
+app.listen(port, () => {
+  console.log(`running on port ${port}`);
+});
