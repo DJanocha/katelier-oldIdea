412987a02c7a63c7e63f0e4162b2f759333e5395
przykladowy uzytkownik <przykladowy@email.com>
2022-06-19 22:31:52 +0200

    added categories and removed achievements

--- backend/src/controllers/achievementsController.ts
+++
@@ -1,9 +0,0 @@
-import Achievement from '../models/achievement';
-import { generateHandlers } from '../utils/handlersGenerator';
-// const catchAsync = () => {}
-
-const { createOne, deleteOne, getAll, getOne, updateOne } = generateHandlers({
-  model: Achievement
-});
-
-export { getAll, createOne, getOne, updateOne, deleteOne };
