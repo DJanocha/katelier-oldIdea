4c716e92d09f864d81517900f0ce22a302b86938
przykladowy uzytkownik <przykladowy@email.com>
2022-07-25 21:55:56 +0200

    moved categories API to service layer

--- backend/src/tests/categories.test.ts
+++ backend/src/tests/categories.test.ts
@@ -1,6 +1,7 @@
+import { Types } from 'mongoose';
 import { Category, Project } from 'src/models';
-import { CategoryDocument } from 'src/models/categories';
 import { connectDB, clearDB, closeDB } from './db';
+import { addProject } from 'src/services/categoriesService';
 
 beforeAll(async () => await connectDB());
 afterEach(async () => await clearDB());
@@ -11,14 +12,18 @@ const secondCategoryName = 'second category';
 const firstProjectName = 'first project name';
 describe('adding new project', () => {
   let projectsCountBefore: number;
+  let firstCategoryId: Types.ObjectId;
+  let secondCategoryId: Types.ObjectId;
 
   beforeEach(async () => {
     const firstCategory = new Category({ name: firstCategoryName });
     const firstProject = new Project({ name: firstProjectName, category: firstCategory._id });
     firstCategory.projects.push(firstProject._id);
+    firstCategoryId = firstCategory._id;
     await firstProject.save();
     await firstCategory.save();
     const secondCategory = new Category({ name: secondCategoryName });
+    secondCategoryId = secondCategory._id;
     await secondCategory.save();
     projectsCountBefore = await Project.find().count();
   });
@@ -26,27 +31,23 @@ describe('adding new project', () => {
   describe('given project name', () => {
     describe('given already occupied project name for given category', () => {
       it('Should NOT let create new project', async () => {
-        const category = await Category.findOne<CategoryDocument>({ name: firstCategoryName });
+        await expect(addProject(firstCategoryId, firstProjectName)).rejects.toThrow();
 
-        await expect(category?.addProject(firstProjectName)).rejects.toThrow();
         const projectsAfter = await Project.find().count();
         expect(projectsAfter).toEqual(projectsCountBefore);
       });
     });
     describe('given project name NOT occupied yet by given category ', () => {
       it('Should create new project', async () => {
-        const category = await Category.findOne<CategoryDocument>({ name: secondCategoryName });
-
-        await expect(category?.addProject(firstProjectName)).resolves.not.toThrow();
+        await expect(addProject(secondCategoryId, firstProjectName)).resolves.not.toThrow();
         const projectsAfter = await Project.find().count();
         expect(projectsAfter).toEqual(projectsCountBefore + 1);
       });
     });
     describe('given project name used by OTHER category', () => {
       it('Should create new project', async () => {
-        const category = await Category.findOne<CategoryDocument>({ name: secondCategoryName });
 
-        await expect(category?.addProject(firstProjectName)).resolves.not.toThrow();
+        await expect(addProject(secondCategoryId, firstProjectName)).resolves.not.toThrow();
         const projectsAfter = await Project.find().count();
         expect(projectsAfter).toEqual(projectsCountBefore + 1);
       });
