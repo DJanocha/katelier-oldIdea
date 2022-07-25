943ed67f03fb4c60821c8332efb3e1b9e43e0bef
przykladowy uzytkownik <przykladowy@email.com>
2022-07-25 23:46:55 +0200

    requiring userid part1

--- backend/src/services/categoriesService.ts
+++ backend/src/services/categoriesService.ts
@@ -1,11 +1,13 @@
 import { Types } from 'mongoose';
 import { Category, CategoryDocument } from 'src/models/categories';
+import { User, UserDocument } from 'src/models/users';
 import { AppError } from 'src/utils';
 
-export const addProject = async (categoryId: Types.ObjectId, newProjectName: string) => {
+export const addProject = async (userId : Types.ObjectId,categoryId: Types.ObjectId, newProjectName: string) => {
   const category: CategoryDocument | null = await Category.findById<CategoryDocument>(categoryId);
+  const user : UserDocument | null = await User.findById<UserDocument>(userId)
 
-  if (!category) {
+  if (!category || !user) {
     throw new AppError('could not find the category', 400);
   }
 
--- backend/src/tests/categories.test.ts
+++ backend/src/tests/categories.test.ts
@@ -2,11 +2,14 @@ import { Types } from 'mongoose';
 import { Category, Project } from 'src/models';
 import { connectDB, clearDB, closeDB } from './db';
 import { addProject } from 'src/services/categoriesService';
+import { register } from 'src/services/authService';
 
 beforeAll(async () => await connectDB());
 afterEach(async () => await clearDB());
 afterAll(async () => await closeDB());
 
+const validPassword = 'dupadupa';
+const takenEmail = 'emailfortest222222@test.test';
 const firstCategoryName = 'first category';
 const secondCategoryName = 'second category';
 const firstProjectName = 'first project name';
@@ -14,8 +17,11 @@ describe('adding new project', () => {
   let projectsCountBefore: number;
   let firstCategoryId: Types.ObjectId;
   let secondCategoryId: Types.ObjectId;
+  let userId: Types.ObjectId;
 
   beforeEach(async () => {
+    const user = await register({ email: takenEmail, password: validPassword, passwordConfirm: validPassword });
+    userId = user._id;
     const firstCategory = new Category({ name: firstCategoryName });
     const firstProject = new Project({ name: firstProjectName, category: firstCategory._id });
     firstCategory.projects.push(firstProject._id);
@@ -31,7 +37,7 @@ describe('adding new project', () => {
   describe('given project name', () => {
     describe('given already occupied project name for given category', () => {
       it('Should NOT let create new project', async () => {
-        await expect(addProject(firstCategoryId, firstProjectName)).rejects.toThrow();
+        await expect(addProject(userId,firstCategoryId, firstProjectName)).rejects.toThrow();
 
         const projectsAfter = await Project.find().count();
         expect(projectsAfter).toEqual(projectsCountBefore);
@@ -39,7 +45,7 @@ describe('adding new project', () => {
     });
     describe('given project name NOT occupied yet by given category ', () => {
       it('Should create new project', async () => {
-        await expect(addProject(secondCategoryId, firstProjectName)).resolves.not.toThrow();
+        await expect(addProject(userId,secondCategoryId, firstProjectName)).resolves.not.toThrow();
         const projectsAfter = await Project.find().count();
         expect(projectsAfter).toEqual(projectsCountBefore + 1);
       });
@@ -47,7 +53,7 @@ describe('adding new project', () => {
     describe('given project name used by OTHER category', () => {
       it('Should create new project', async () => {
 
-        await expect(addProject(secondCategoryId, firstProjectName)).resolves.not.toThrow();
+        await expect(addProject(userId,secondCategoryId, firstProjectName)).resolves.not.toThrow();
         const projectsAfter = await Project.find().count();
         expect(projectsAfter).toEqual(projectsCountBefore + 1);
       });
