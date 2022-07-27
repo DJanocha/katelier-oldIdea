35f3fdeabf86c07ec491c2f8d891fc68c054a37c
przykladowy uzytkownik <przykladowy@email.com>
2022-07-27 21:19:25 +0200

    completing step service

--- backend/src/services/stepService.ts
+++ backend/src/services/stepService.ts
@@ -1,5 +1,5 @@
 import { Types } from 'mongoose';
-import { Activity, Project, Step } from 'src/models';
+import { Activity, IActivity, IStep, Project, Step } from 'src/models';
 import { ProjectDocument } from 'src/models/projects';
 import { AppError } from 'src/utils';
 export const addStep = async ({
@@ -21,9 +21,9 @@ export const addStep = async ({
   description?: string;
   userId: Types.ObjectId;
 }) => {
-  const project : ProjectDocument | null = await Project.findById(projectId);
-  if(!project){
-    throw new AppError('could not find project. try to add step again.',400)
+  const project: ProjectDocument | null = await Project.findById(projectId);
+  if (!project) {
+    throw new AppError('could not find project. try to add step again.', 400);
   }
   const innerActivity = new Activity({ start_time, stop_time, date, description, userId });
   const step = new Step({ category: categoryId, project: projectId, step_number: project?.steps?.length || -1, img });
@@ -32,5 +32,24 @@ export const addStep = async ({
   await step.save();
   await innerActivity.save();
   project.steps.push(step._id);
-  await project.save()
+  await project.save();
+};
+
+type StepMutation = Pick<IActivity, 'date' | 'start_time' | 'stop_time' | 'description'> &
+  Pick<IStep, 'img' | 'used_materials'> & {
+    stepId: Types.ObjectId;
+  };
+
+export const updateStep = async ({ stepId, ...data }: StepMutation) => Step.findByIdAndUpdate(stepId, data);
+
+export const deleteStep = async (stepId: Types.ObjectId) => Step.findByIdAndDelete(stepId);
+
+export const getStep = async (stepId: Types.ObjectId) => Step.findById(stepId);
+
+export const getAllSteps = async (projectId: Types.ObjectId) => {
+  const project: ProjectDocument | null = await Project.findById(projectId);
+  if (!project) {
+    throw new AppError('could not find the project', 400);
+  }
+  return project.steps;
 };
