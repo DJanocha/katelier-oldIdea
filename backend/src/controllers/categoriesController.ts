387d9020a7ee04ca6a3db8e75697a080123ffcf7
przykladowy uzytkownik <przykladowy@email.com>
2022-08-07 20:27:32 +0200

    categoryService.removeCategory ready

--- backend/src/controllers/categoriesController.ts
+++ backend/src/controllers/categoriesController.ts
@@ -1,9 +1,10 @@
 import { RequestHandler } from 'express';
 import { Category } from 'src/models';
 import { catchAsync, generateHandlers } from 'src/utils';
+import { Types } from 'mongoose'
 import * as service from 'src/services/categoriesService';
 
-const { createOne, deleteOne, getOne, updateOne } = generateHandlers({
+const { createOne, getOne, updateOne } = generateHandlers({
   model: Category
 });
 
@@ -18,4 +19,13 @@ const getAll: RequestHandler = catchAsync(async (req, res, next) => {
   const categories = await service.getUsersCategories( user?._id);
   return res.status(200).json({ ok: true, data: { categories } });
 });
+
+const deleteOne: RequestHandler = catchAsync(async (req, res, next) => {
+  const { params } = req;
+  const { id } = params
+  
+  await service.removeCategory(new Types.ObjectId(id));
+  return res.status(200).json({ ok: true });
+});
+
 export { addCategory, createOne, deleteOne, getAll, getOne, updateOne };
