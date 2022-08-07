55e1a4f4ce7d0624815d5abee2a69dca68569c96
przykladowy uzytkownik <przykladowy@email.com>
2022-08-07 21:42:27 +0200

    removing unused routes

--- backend/src/routes/categoriesRouter.ts
+++ backend/src/routes/categoriesRouter.ts
@@ -18,40 +18,5 @@ router.route('/new').post(addCategory);
 
 router.route('/:id').get(getOne).delete(deleteOne).patch(restrictInvalidProjects, updateOne);
 
-router.route('/:categoryName/populateSteps').get(
-  catchAsync(async (req, res, next) => {
-    console.log({ r: req.params });
-    const steps = await Step.find().populate({ path: 'project' });
-    return res.json({ ready: false, steps });
-  })
-);
-router.route('/:categoryName/projects/').get(
-  catchAsync(async (req, res, next) => {
-    const { categoryName } = req.params;
-    if (!categoryName) {
-      return next(new AppError('invalid path, try other one', 404));
-    }
-    const projects = await Project.find({ categoryName });
-    const allProjects = await Project.find();
-    return res.json({
-      results: projects.length,
-      projects,
-      allProjects,
-      allProjectsLength: allProjects.length
-    });
-  })
-);
-router.route('/:categoryName/projects/:projectName/steps').get(
-  catchAsync(async (req, res, next) => {
-    const { categoryName, projectName } = req.params;
-    if (!categoryName || !projectName) {
-      return next(new AppError('invalid path, try other one', 404));
-    }
-    const steps = await Step.find({
-      categoryName,
-      projectName
-    });
-    return res.json({ results: steps.length, steps });
-  })
-);
+router.use('/:categoryId/projects', projectsRouter);
 export default router;
